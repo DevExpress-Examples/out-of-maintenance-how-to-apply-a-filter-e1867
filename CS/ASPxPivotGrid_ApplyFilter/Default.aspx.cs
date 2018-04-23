6dbbb874ca7b08f1b8720de8a46b73b8f1dd319f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/ASPxPivotGrid_ApplyFilter/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DevExpress.Data.PivotGrid;
+using DevExpress.Web.ASPxPivotGrid;
+
+namespace ApplyFilter {
+    public partial class _Default : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+            if (!IsPostBack && !IsCallback) {
+                PivotGridField field = ASPxPivotGrid1.Fields["Country"];
+                // Locks the control to prevent excessive updates
+                // when multiple properties are modified.
+                ASPxPivotGrid1.BeginUpdate();
+                try {
+                    // Clears the filter value collection and add two items to it.
+                    field.FilterValues.Clear();
+                    field.FilterValues.Add("Brazil");
+                    field.FilterValues.Add("USA");
+                    // Specifies that the control should only display the records 
+                    // which contain the specified values in the Country field.
+                    field.FilterValues.FilterType = PivotFilterType.Included;
+                }
+                finally {
+                    // Unlocks the control.
+                    ASPxPivotGrid1.EndUpdate();
+                }
+            }
+        }
+    }
+}
---
+++ VB/ASPxPivotGrid_ApplyFilter/Default.aspx.vb
@@ -0,0 +1,28 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Web.ASPxPivotGrid
+
+Namespace ApplyFilter
+	Partial Public Class _Default
+		Inherits System.Web.UI.Page
+		Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+			If (Not IsPostBack) AndAlso (Not IsCallback) Then
+				Dim field As PivotGridField = ASPxPivotGrid1.Fields("Country")
+				' Locks the control to prevent excessive updates when multiple properties are modified.
+				ASPxPivotGrid1.BeginUpdate()
+				Try
+					' Clears the filter value collection and add two items to it.
+					field.FilterValues.Clear()
+					field.FilterValues.Add("Brazil")
+					field.FilterValues.Add("USA")
+					' Specifies that the control should only display the records 
+					' which contain the specified values in the Country field.
+					field.FilterValues.FilterType = DevExpress.Data.PivotGrid.PivotFilterType.Included
+				Finally
+					' Unlocks the control.
+					ASPxPivotGrid1.EndUpdate()
+				End Try
+			End If
+		End Sub
+	End Class
+End Namespace
